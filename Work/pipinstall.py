3899d9301c7572eda8a87ecdda5349962903ed55
prateek singh kushwaha <prateek20205@gmail.com>
2025-06-23 18:35:08 +0530

    removing some files

--- Work/pipinstall.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-base_url = "http://www.omdbapi.com/"
-
-params = {"T": "main hoon na", "apikey": "d95bdd9a"}
-
-response = requests.get(base_url, params=params)
-print(response.text)
